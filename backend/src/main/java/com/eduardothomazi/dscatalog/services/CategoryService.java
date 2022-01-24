f7238e0e3e7d773e636a9526d6cfaee6a57408b1
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-01-23 21:55:15 -0300

    Database seeding

--- backend/src/main/java/com/eduardothomazi/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eduardothomazi/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import com.eduardothomazi.dscatalog.entities.Category;
 import com.eduardothomazi.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
